896f8e4c82807031e5756d397b66053c2718d59a
KenjiArdianto <kenjiardianto@gmail.com>
2025-07-04 23:21:48 +0700

    Push Login Page and About Us Page - Kurang Intent dari Login ke Homepage - Kurang Navbar

---
+++ app/src/main/java/com/example/uxlab_animedxd/AboutUsActivity.java
@@ -0,0 +1,79 @@
+package com.example.uxlab_animedxd;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    private TextView welcomeText;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            // Handle logout
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_about_us);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        androidx.appcompat.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+//        actionBar.setDisplayShowHomeEnabled(true);
+//        actionBar.setIcon(R.drawable.logo_with_text);
+
+        actionBar.setDisplayShowCustomEnabled(true);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.custom_actionbar, null);
+        actionBar.setCustomView(view);
+
+        SharedPreferences prefs = getSharedPreferences("user_prefs", MODE_PRIVATE);
+        String username = prefs.getString("username", "");
+
+        welcomeText = findViewById(R.id.welcomeText);
+
+        String welcomeMessage = "Welcome, " + username;
+
+        welcomeText.setText(welcomeMessage);
+
+    }
+}
---
+++ app/src/main/java/com/example/uxlab_animedxd/LoginActivity.java
@@ -0,0 +1,84 @@
+package com.example.uxlab_animedxd;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private TextView feedbackText1;
+    private TextView feedbackText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+//        bagian template default
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+//        sambungin ke object di view
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        feedbackText1 = findViewById(R.id.feedbackText1);
+        feedbackText2 = findViewById(R.id.feedbackText2);
+        Button loginButton = findViewById(R.id.loginButton);
+
+
+//        ketika button dipencet
+        loginButton.setOnClickListener(v -> {
+            String username = usernameEditText.getText().toString().trim();
+            String password = passwordEditText.getText().toString().trim();
+
+            // Hide previous error first
+            feedbackText1.setVisibility(View.INVISIBLE);
+            feedbackText2.setVisibility(View.INVISIBLE);
+
+            if (username.isEmpty()) {
+                feedbackText1.setText("Username must be filled in");
+                feedbackText1.setVisibility(View.VISIBLE);
+
+            } else if (password.isEmpty()) {
+                feedbackText1.setText("Password must be filled in");
+                feedbackText1.setVisibility(View.VISIBLE);
+
+            } else if (username.length() > 10 || username.length() < 5) {
+
+                feedbackText1.setText("Length of username must be");
+                feedbackText2.setText("5 - 10 characters (inclusive)");
+
+                feedbackText1.setVisibility(View.VISIBLE);
+                feedbackText2.setVisibility(View.VISIBLE);
+
+
+
+            } else {
+                SharedPreferences prefs = getSharedPreferences("user_prefs", MODE_PRIVATE);
+                prefs.edit().putString("username", username).apply();
+
+//                buat pindah page
+                Intent intent = new Intent(LoginActivity.this, AboutUsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
+    }
+}
